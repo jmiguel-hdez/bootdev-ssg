cff7515ead0215d779b96cf94fb8cc8d4b7dff76
miguel <jmiguel.hdez@gmail.com>
2025-03-17 12:25:14 -0500

    implement HTMLNode

---
+++ src/htmlnode.py
@@ -0,0 +1,26 @@
+class HTMLNode():
+    def __init__(self, tag: str=None, value: str=None, children:list =None, props: dict=None):
+        """Initialize an HTMLNode instance.
+
+        Args:
+            tag (str, optional): A string representing the HTML tag name (e.g. "p", "a", "h1", etc.)
+            value (str, optional): A string representing the value of the HTML tag (e.g. the text inside a paragraph)
+            children (list[HTMLNode], optional): A list of HTMLNode objects representing the children of this node
+            props (dict, optional): A dictionary of key-value pairs representing the attributes of the HTML tag. 
+                                    For example, a link (<a> tag) might have {"href": "https://www.google.com"}
+        """
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        props = [f'{k}="{v}"' for k,v in self.props.items()]
+        return " ".join(props)
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.tag!r},{self.value!r},{self.children!r},{self.props!r})"
+    
---
+++ src/test_htmlnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_repr(self):
+        node = HTMLNode("p", "a paragraph text")
+        representation = str(node)
+        print(node)
+        print(representation)
+        self.assertTrue(representation.find("HTMLNode(") != -1)
+
+    def test_props(self):
+        node = HTMLNode("a", "google.com", None, {"href":"https://www.google.com", "target":"_blank"})
+        props = node.props_to_html()
+        expect = 'href="https://www.google.com" target="_blank"'
+        self.assertEqual(props, expect)
+
+    def test_to_html(self):
+        node = HTMLNode("a", "google.com", None, {"href":"https://www.google.com", "target":"_blank"})
+        self.assertRaises(NotImplementedError, node.to_html)
+
+    def test_defaults(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
