4471ba82e5aa886371bbfb600ac7b0a475a261e8
miguel <jmiguel.hdez@gmail.com>
2025-03-17 15:02:01 -0500

    add test case for no tag

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -76,6 +76,10 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_value(self):
         node = LeafNode("p", None)
         self.assertRaises(ValueError, node.to_html)
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
