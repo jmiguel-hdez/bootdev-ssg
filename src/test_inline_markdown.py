9a956628c52e63dc5025513db72e18df463eef15
miguel <jmiguel.hdez@gmail.com>
2025-03-18 19:26:19 -0500

    add more tests

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -186,5 +186,32 @@ class TestSplitNodes(unittest.TestCase):
         )
 
 
+    def test_split_images_no_image(self):
+        node = TextNode(
+            "This is text with an [image](https://i.imgur.com/zjjcJKZ.png) and another",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                node
+            ],
+            new_nodes,
+        )
+
+    def test_split_links_no_link(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                node
+            ],
+            new_nodes,
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
